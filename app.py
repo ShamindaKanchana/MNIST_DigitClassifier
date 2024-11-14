f68940c5d0e628c5bcc070711f1faf5b4cdedeb1
ShamindaKanchana <msk60310910@gmail.com>
2024-11-14 14:31:47 +0530

    Created basic view for the app

--- app.py
+++ app.py
@@ -8,6 +8,25 @@ import cv2
 WIN_SIZEX=600
 WIN_SIZEY=450
 
+
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+RED=(255,0,0)
+
+IMAGESAVE=False
+
+MODEL=load_model("model.h5")
+
+LABELS={0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine"}
+
 #initialize 
 pygame.init()
 pygame.display.set_mode((WIN_SIZEX,WIN_SIZEY))
+
+#FONT=pygame.font.Font('freesansbold.tff',18)
+pygame.display.set_caption('Digit Board')
+while True:
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            sys.exit()
